3e4abb7fcdd161c5347eb349e1709bd7f09308ad
Paul <pkilgarriff@gmail.com>
2022-02-09 17:34:59 +0000

    Refactor to handle multiples of both 5 and 3

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -9,7 +9,8 @@
 =end
   
 def fizzbuzz(number)
-  return "fizzbuzz" if number == 15 || number == 30
+  return "fizzbuzz" if number % 5 == 0 && number % 3 == 0
+  # return "fizzbuzz" if number % 15 == 0
   return "fizz" if number % 3 == 0 
   return "buzz" if number == 5
   number
