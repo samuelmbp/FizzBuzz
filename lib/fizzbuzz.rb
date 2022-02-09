5a1805e4ebca7572be3123af051150f096139974
Paul <pkilgarriff@gmail.com>
2022-02-09 17:41:11 +0000

    Update buzz logic to pass 10 returns 'buzz'

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -11,6 +11,6 @@
 def fizzbuzz(number)
   return "fizzbuzz" if number % 15 == 0
   return "fizz" if number % 3 == 0 
-  return "buzz" if number == 5
+  return "buzz" if number == 5 || number == 10
   number
 end
