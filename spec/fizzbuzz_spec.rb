ae1188e5abe6cb437495442e0db0bfb8bda224e5
Paul <pkilgarriff@gmail.com>
2022-02-09 16:51:39 +0000

    Add test for non-3 numbers

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,7 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  it 'returns the input when not passed 3' do
+    expect(fizzbuzz(4)).to eq 4
+  end
 end
