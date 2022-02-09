bcfc75c88e5e19534d562468eb54190eca179174
Samuel Raducan <samuel.thembp@gmail.com>
2022-02-09 17:09:36 +0000

    Add test for buzz

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,4 +7,7 @@ describe 'fizzbuzz' do
   it 'returns the input when not passed 3' do
     expect(fizzbuzz(4)).to eq 4
   end
+   it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
 end
