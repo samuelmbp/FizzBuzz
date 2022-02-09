cae15592177f6e49fdf91ad519ac758a252bc944
Samuel Raducan <samuel.thembp@gmail.com>
2022-02-09 17:15:21 +0000

    Add test for fizzbuzz=15

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,10 +4,16 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  
   it 'returns the input when not passed 3' do
     expect(fizzbuzz(4)).to eq 4
   end
-   it 'returns "buzz" when passed 5' do
+  
+  it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
 end
