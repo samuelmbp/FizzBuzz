d4ffb568c87f815c4b54610a7838ea71ed4da114
Paul <pkilgarriff@gmail.com>
2022-02-09 17:39:09 +0000

    Refactor fizzbuzz logic further as multiples of 15

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -9,8 +9,7 @@
 =end
   
 def fizzbuzz(number)
-  return "fizzbuzz" if number % 5 == 0 && number % 3 == 0
-  # return "fizzbuzz" if number % 15 == 0
+  return "fizzbuzz" if number % 15 == 0
   return "fizz" if number % 3 == 0 
   return "buzz" if number == 5
   number
