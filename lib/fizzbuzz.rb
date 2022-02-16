7c77ee9023c0cddd6569dcc7446e5763aaa180ff
Samuel Raducan <samuel.thembp@gmail.com>
2022-02-16 09:48:11 +0000

    Refactor fizzbuzz.rb file by adding '% 3 and % 5'

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -12,7 +12,7 @@
 # [*1..100].each { |num| puts fizzbuzz(num) }
 
 def fizzbuzz(number)
-  return "fizzbuzz" if number % 15 == 0
+  return "fizzbuzz" if number % 3 == 0 && number % 5 == 0
   return "fizz" if number % 3 == 0 
   return "buzz" if number % 5 == 0
   number
